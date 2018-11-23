6e981d605467e845c17a4c33c748fc6f5980a68c
JackKiefer <jack.c.kiefer@gmail.com>
2018-11-23 16:21:35 -0700

    Add readme

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ print("Beginning martian-trail setup...")
 if bmcd.test_mcd():
     pnp.pickleScenario('dust', DENA_LAT, DENA_LON)
     pnp.pickleScenario('climatology', DENA_LAT, DENA_LON)
+    print("Finished!")
 
-print("Finished!")
+
